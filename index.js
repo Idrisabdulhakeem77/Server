cd0cf0a73cb3a16f7d4c4cef15174ae97f5bd692
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2023-06-28 07:35:30 +0100

    added a not found middleware

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
+const notFoundMiddleware = require('./middleware/not-found')
 
 
 
 const blogs = require('./routes/blog')
 
 app.use('/api/v1/blogs' , blogs)
+
+
+
+app.use(notFoundMiddleware)
 
 
 const port = process.env.PORT || 5000;
